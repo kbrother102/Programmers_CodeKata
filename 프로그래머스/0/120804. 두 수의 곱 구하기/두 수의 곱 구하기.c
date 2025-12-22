aaa61b10fa8d1be60856a405d3abf041bcf8b0dc
kbrother102 <kbrother102@gmail.com>
2025-12-22 09:16:29 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    
+if( 0<=num1&&num1<=100 &&   0<=num2&&num2<=100)
+    {
+        int answer = num1*num2;
+        return answer; 
+    }
+}
