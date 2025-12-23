8d84464629c8bb3073cbf9390cee57489a56ed26
kbrother102 <kbrother102@gmail.com>
2025-12-23 09:19:15 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    if(num1<= 50000 && num1>=-50000 && num2<= 50000 && num2>=-50000 )
+    {
+    
+     return num1+num2;
+         
+    }
+    
+}
