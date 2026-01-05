958fa7012de0a813b7d464a24425f383fcdfc573
kbrother102 <kbrother102@gmail.com>
2026-01-05 09:57:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,25 @@
+int solution(int num) {
+    int answer = 0;
+    int cnt = 0 ;
+
+    while (num != 1)
+    {
+        if (cnt > 500)
+        {
+            return -1;
+        }
+
+        if (num % 2 == 0)
+        {
+            num = num / 2;
+        }
+        else if (num % 2 == 1)
+        {
+            num = num * 3 + 1;
+        }
+
+        
+        cnt++;
+    }
+    return cnt;
+}
